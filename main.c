18b3cb02787b33a6021ea6e88ea19949a8c3a989
Leoruizdev <leonardo.mruiz7@outlook.com>
2025-05-28 18:19:57 -0300

    main

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h> 
+#include "estruturas.h"
 
 int main(){
 
---
+++ simulador.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "estruturas.h"
+
+Simulador *inicializar_simulador(int tamanho_pagina, int tamanho_memoria_fisica){
+    
+}
