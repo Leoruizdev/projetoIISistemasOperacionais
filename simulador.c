cfcd511d01176027aac4f9beb7d076c69d8873f6
Leoruizdev <leonardo.mruiz7@outlook.com>
2025-05-28 21:53:30 -0300

    simulador

--- simulador.c
+++ simulador.c
@@ -48,6 +48,66 @@ Processo *criar_processo(Simulador *sim, int tamanho_processo, int pid){
     return proc;
 }
 
+void extrair_pagina_deslocamento ( Simulador *sim, int endereco_virtual, int *pagina ,int *deslocamento){
+    *pagina = endereco_virtual / sim->tamanho_pagina;
+    *deslocamento = endereco_virtual & sim->tamanho_pagina;
+}
+
+int traduzir_endereco(Simulador *sim, int pid, int endereco_virtual){
+    sim->total_acessos++;
+
+    int pagina, deslocamento;
+
+    extrair_pagina_deslocamento(sim, endereco_virtual, &pagina, &deslocamento);
+
+    Processo *proc = NULL;
+    for (int i = 0; i < sim->num_processos; i++){
+        if(sim->num_processos[i].pid == pid){
+            proc = &sim->processos;
+            break;
+        }
+    }
+
+    if(!proc) return -1; // processo n encontrado.
+
+    if(proc->tabela_paginas[pagina].presente){
+        registrar_acesso(sim, pid, pagina, 0); // tipo_acesso = leitura
+        proc->tabela_paginas[pagina].ultimo_acesso = sim->tempo_atual;
+        return proc->tabela_paginas[pagina].frame * sim->tamanho_pagina + deslocamento;
+    }
+
+    //page fault
+    sim->page_faults++;
+    
+    int frame;
+    switch (sim->algoritmo)
+    {
+    case 0:
+        frame = substituir_pagina_fifo(sim);
+        break;
+    case 1: 
+        frame = substituir_pagina_lru(sim);
+        break;
+    default:
+        printf("Opção Inválida. Usaremos como padrão o FIFO");
+        frame = substituir_pagina_fifo(sim);
+    }
+
+    sim->memoria.frames[frame] = (pid << 16) | pagina;
+    sim->memoria.tempo_carga[frame] = sim->tempo_atual;
+
+    // Atualizar a tabela de páginas do processo
+    proc->tabela_paginas[pagina].presente = 1;
+    proc->tabela_paginas[pagina].frame = frame;
+    proc->tabela_paginas[pagina].tempo_carga = sim->tempo_atual;
+    proc->tabela_paginas[pagina].ultimo_acesso = sim->tempo_atual;
+
+    registrar_acesso(sim, pid, pagina, 1); // tipo_acesso = escrita (por substituição)
+
+    return frame * sim->tamanho_pagina + deslocamento;
+    
+}
+
 void exibir_memoria_fisica(Simulador *sim) {
     printf("Estado da Memória Física:\n");
     for (int i = 0; i < sim->memoria.num_frames; i++) {
